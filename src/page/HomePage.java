2919e55136610f0aa86caeb2f2d03b9ba0555182
arpitamathad <43536406+arpitamathad@users.noreply.github.com>
2018-10-01 11:20:38 +0530

    updated

---
+++ src/page/HomePage.java
@@ -0,0 +1,22 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+
+	private WebDriver driver;
+	@FindBy(id="logoutLink")
+	private WebElement logoutLink;
+	
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void clickLogout() {
+		logoutLink.click();
+	}
+}
